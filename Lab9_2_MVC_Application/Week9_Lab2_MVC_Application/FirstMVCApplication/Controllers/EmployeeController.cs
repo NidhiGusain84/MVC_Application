50ecd0c997c38be7453c3729eb4ed516d1199116
Gusain, Nidhi <NidhiGusain@quickenloans.com>
2021-09-10 00:10:06 -0400

    first commit

---
+++ Lab9_2_MVC_Application/Week9_Lab2_MVC_Application/FirstMVCApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using FirstMVCApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult EmployeeForm()
+        {
+            
+            return View();
+        }
+        public IActionResult Thankyou(Employee employee)
+        {
+            return View(employee);
+        }
+    }
+}
---
+++ Lab9_2_MVC_Application/Week9_Lab2_MVC_Application/FirstMVCApplication/Controllers/MortgageController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstMVCApplication.Controllers
+{
+    public class MortgageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPage()
+        {
+            ViewData["FirstName"] = "Nidhi";
+            ViewData["LastName"] = "Gusain";
+            return View();
+        }
+
+        public IActionResult DisplayForm()
+        {
+            return View();
+        }
+        public IActionResult ResponsePage(string firstName, string lastName, string email)
+        {
+            ViewData["FirstName"] = firstName;
+            ViewData["LastName"] = lastName;
+            ViewData["Email"] = email;
+            return View();
+        }
+
+    }
+}
